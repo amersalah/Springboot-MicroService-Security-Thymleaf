8a93e7f6785335153f95aced8871fad1424be183
Amer Salah <amer71475@gmail.com>
2019-07-02 21:32:29 -0700

    implementing JDBC Authentication

---
+++ guest-app/src/main/java/com/frankmoley/security/app/auth/LondonUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.frankmoley.security.app.auth;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LondonUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public LondonUserDetailsService(UserRepository userRepository) {
+        super();
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepository.findByUsername(username);
+        if (user ==null)
+            throw new UsernameNotFoundException("can't find username "+username);
+        return new LondonUserPrinciple(user);
+    }
+}
--- guest-app/src/main/java/com/frankmoley/security/app/security_config/ApplicationSecurityConfiguration.java
+++ guest-app/src/main/java/com/frankmoley/security/app/security_config/ApplicationSecurityConfiguration.java
@@ -1,13 +1,19 @@
 package com.frankmoley.security.app.security_config;
 
+import com.frankmoley.security.app.auth.LondonUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 import java.util.ArrayList;
@@ -17,20 +23,28 @@ import java.util.List;
 @EnableWebSecurity
 public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private LondonUserDetailsService userDetailsService;
+
+    @Bean
+    public DaoAuthenticationProvider authenticateProvider()
+    {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService);
+        provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+        return provider;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(authenticateProvider());
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests().antMatchers("/" , "/index" , "/css/*" , "/js/*").permitAll()
                 .anyRequest().authenticated()
                 .and().httpBasic();
-    }
-
-    @Override
-    @Bean
-    public UserDetailsService userDetailsService() {
-        List<UserDetails> users = new ArrayList<>();
-        users.add((User.withDefaultPasswordEncoder().username("Amer").password("28199612").roles("USER" , "Admin").build()));
-        users.add((User.withDefaultPasswordEncoder().username("Ahmed").password("12345").roles("USER").build()));
-        return new InMemoryUserDetailsManager(users);
     }
 }
