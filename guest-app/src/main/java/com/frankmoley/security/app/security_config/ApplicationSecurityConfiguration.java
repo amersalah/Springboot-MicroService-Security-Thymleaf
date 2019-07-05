8e78a1d1b0212167b7c73391787475134a130a22
Amer Salah <amer71475@gmail.com>
2019-07-04 23:02:10 -0700

    implementing Hashing with BCryptPasswordEncoder

--- guest-app/src/main/java/com/frankmoley/security/app/security_config/ApplicationSecurityConfiguration.java
+++ guest-app/src/main/java/com/frankmoley/security/app/security_config/ApplicationSecurityConfiguration.java
@@ -13,6 +13,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
@@ -31,7 +32,7 @@ public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapt
     {
         DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
         provider.setUserDetailsService(userDetailsService);
-        provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+        provider.setPasswordEncoder(new BCryptPasswordEncoder(11));
         return provider;
     }
 
